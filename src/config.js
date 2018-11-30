8e1bc658e7daba643d3a274b671a6839e58f89e5
Sergei Garcia Ortega <sgarcia.dev@gmail.com>
2018-11-29 21:42:38 -0600

    Config change

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  process.env.NODE_ENV === 'production' ? 'https://react-image-upload.herokuapp.com' : 'http://localhost:8080';
+  process.env.NODE_ENV === 'production' ? 'https://image-uploader-server.herokuapp.com' : 'http://localhost:8080';
